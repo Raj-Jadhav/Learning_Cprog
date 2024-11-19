540c6883ee577e2b83def901f09f5dc1dd94c5c9
Raj-Jadhav <jadxur@gmail.com>
2024-11-19 09:17:40 -0800

    Added Class 8 to the repo on 19-11-2024

---
+++ Class8.0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int score = 95;
+
+    if (score > 80){
+        printf("Your grade is  a high distinction");
+
+    }
+    else {
+    printf("your grade is a pass");
+    }
+
+return 0;
+}
