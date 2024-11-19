3a5a8fd2103ebb28f7f0df743bfeeac916b830fd
Raj-Jadhav <jadxur@gmail.com>
2024-11-19 09:43:38 -0800

    Added the compiled output files & executable files for class 8 and 7

--- class7.1.c
+++ class7.1.c
@@ -7,7 +7,7 @@ int main(){
     if(a>h){
     //if (a==h){
     //if (a!=h){
-        printf("a is equal to h");
+        printf("a is equal to h\n");
     }
     else{
         printf("a is not equal to h");
