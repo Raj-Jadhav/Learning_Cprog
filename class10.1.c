43ac1801dd4807d4c76167b9cee716634677546c
Raj-Jadhav <jadxur@gmail.com>
2024-11-25 09:59:51 -0800

    Added the class 10.1 to the repo and created an  executable fo it

---
+++ class10.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+   /*
+    int x = 3;
+
+        while(x<5){
+            printf("I am a student\n");
+            ++x;
+        }
+    */
+    int i=1;
+
+    while (i <= 8){
+        printf("%d" ,i);
+        i++;
+    }
+return 0;
+}
