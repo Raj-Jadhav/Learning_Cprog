0bbea186db44795e5eaf9236bcb17e1ea0cbd67b
Raj-Jadhav <jadxur@gmail.com>
2024-11-26 09:34:11 -0800

    Added the class 11.0 to repo and has while loop and created an exe for it

---
+++ class11.0.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+   /*
+    int sum, num1, num2, i;
+
+    i =  1;
+
+    num1 = 1 , num2 = 2;
+    while(i<5){
+        sum = num1 + num2;
+        printf("The sum of %d and %d  is %d\n" ,num1, num2, sum);
+        num1= num1+2;
+        i++;
+     */
+     /*
+     int i = 1;
+     int num1 = 1;
+     int power_of_3;
+
+     while(i<=4){
+        power_of_3 = pow(3 , i);
+        printf("3 power %d is %d\n" ,num1,power_of_3);
+        num1 = num1 + 1;
+        i++;
+        */
+    /*
+        int i = 1;
+        int power = 1;
+        int num1 = 1;
+        int num2 = 3;
+        while(i<=4){
+            power= power * 3;
+            printf("3 power %d is %d\n",num1,power);
+            num1++;
+            i++;
+        }
+        */
+
+
+return 0;
+}
