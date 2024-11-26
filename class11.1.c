f8a5ff34102ac22d91945adb59d41dd0d12cfbdf
Raj-Jadhav <jadxur@gmail.com>
2024-11-26 09:47:54 -0800

    Added the class 11.1 to the repo and also it contained the do-while loop and created an exe for it

---
+++ class11.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int x,y,sum;
+    x=0, y=1;
+        do{
+            sum = x + y;
+            printf("The sum of %d + %d is %d\n ", x,y,sum);
+            x++;
+
+        }
+        while(sum<10);
+
+return 0;
+}
