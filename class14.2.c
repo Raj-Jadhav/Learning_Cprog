654f5ad6b7da557498e15b4b2b67f3e20f639d0c
raj-jadhav <jadxur@gmail.com>
2024-12-03 10:03:18 -0800

    Added class 14.2 and compiled it

--- class14.2.c
+++ class14.2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void name(){
-printf("My name is SRJ\n");
-
+void name(int age,int student_number, char my_school[]){
+printf("My name is SRJ. I am %d years old and My student number is %d.\n My uni is %s\n",age, student_number,my_school);
 
 }
 void nation(){
 printf("I am UGANDAN\n");
+
 }
 int main(){
-    printf("ISBAT UNIVERSITY\n");
-    name();
+    //printf("ISBAT UNIVERSITY\n");
+    name(10,1124564,"ISBAT UNIVERSITY");
     nation();
 return 0;
 }
