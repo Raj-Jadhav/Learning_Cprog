226043983b290f7938993e32050b7ee67d331044
raj-jadhav <jadxur@gmail.com>
2024-12-03 09:40:00 -0800

    Added class 13 from 2/12/2024, class 14 from 3/12/2024 with all their compiled files

---
+++ class14.1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Using functions outside the int main and then calling them inside main
+ void display_ISBAT() {
+    printf{"ISBAT UNIVERSITY"};
+}
+
+int main(){
+    display_ISBAT();
+return 0;
+}
---
+++ class14.2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void name(){
+printf("My name is SRJ\n");
+
+
+}
+void nation(){
+printf("I am UGANDAN\n");
+}
+int main(){
+    printf("ISBAT UNIVERSITY\n");
+    name();
+    nation();
+return 0;
+}
