e1bc7cf045507bdf9ba1704d5cf1a59eb8d687e4
Raj-Jadhav <jadxur@gmail.com>
2024-12-09 10:00:45 -0800

    Added class16.2 to the repo on 9-12-2024

--- class16.2.c
+++ class16.2.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-
+int calc();
 int main(){
 
+   int a = calc(200,300);
+   int b = a + 1000;
+   //printf("The sum is %d\n",a);
+   printf("The sum is %d\n",b);
 return 0;
+}
+int calc(int x, int y){
+    int sum = x + y;
+    return sum;
 }
