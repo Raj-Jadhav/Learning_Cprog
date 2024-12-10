ea68a26f7c063add6bf255241fdd04cb6d0e1cbc
Raj-Jadhav <jadxur@gmail.com>
2024-12-10 10:07:15 -0800

    Added Class17.2 into the repo

---
+++ class17.1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+//Using Arrays
+int main(){
+//char name[50];
+//char name[]="ISBAT";
+/*
+int x[50];
+int x_array[]={1,2,3,4,5,6,10};//in arrays positions are counted  from 0123
+x_array[0]=100;
+printf("The value in position 2 is %d\n",x_array[6]);
+
+
+#include <stdio.h>
+
+int main() {
+    // Initialize the array with values
+    int x_array[] = {1, 2, 3, 4, 5, 6, 10};
+
+    // Get the size of the array
+    int size = sizeof(x_array) / sizeof(x_array[0]);
+
+    // Print all the elements in the array
+    for(int i = 0; i < size; i++) {
+        printf("x_array[%d] = %d\n", i, x_array[i]);
+    }
+
+    return 0;
+}
+*/
+int i;
+for(i=0;i<=5;i++){
+    printf("%d\n",i);
+}
+return 0;
+}
